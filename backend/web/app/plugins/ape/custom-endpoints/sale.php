d500d90b51732af2e1da49eb030521828f469578
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-07-20 16:10:43 +0200

    change comment and return post id when insert post ok

--- backend/web/app/plugins/ape/custom-endpoints/sale.php
+++ backend/web/app/plugins/ape/custom-endpoints/sale.php
@@ -3,7 +3,7 @@
 add_action('rest_api_init', 'ape_rest_sale_register');
 function ape_rest_sale_register()
 {
-    // Definit une nouvelle route pour notre inscription d'utilisateur
+    // custom route when sale added
     register_rest_route('wp/v2', 'sale', array(
         'methods' => 'POST',
         'callback' => 'ape_rest_add_sale_handler',
@@ -13,26 +13,28 @@ function ape_rest_sale_register()
     ));
 }
 
+// add request content to the custom post and add custom post meta
 function ape_rest_add_sale_handler($request)
 {
-    // Preparation de la réponse HTTP
-    $response = [];
-    // Recuperation du formulaire sous un format JSON
+
+    // translate request with JSON
     $parameters = $request->get_json_params();
     $title = sanitize_text_field($parameters['title']);
     $content = sanitize_text_field($parameters['content']);
     $date = sanitize_text_field($parameters['date']);
     $lieu = sanitize_text_field($parameters['lieu']);
     $lien = sanitize_text_field($parameters['lien']);
-    
-    
-        $post_id = wp_insert_post([
-            'post_title' => $title,
-            'post_content' => $content,
-            'post_type' => 'sale'
-        ]);
-        add_post_meta($post_id, 'lieu', $lieu);
-        add_post_meta($post_id, 'lien', $lien);
-        add_post_meta($post_id, 'date', $date);
-    return new WP_REST_Response($response, 123);
+
+    // add to the database
+    $post_id = wp_insert_post([
+        'post_title' => $title,
+        'post_content' => $content,
+        'post_type' => 'sale'
+    ]);
+    add_post_meta($post_id, 'lieu', $lieu);
+    add_post_meta($post_id, 'lien', $lien);
+    add_post_meta($post_id, 'date', $date);
+
+    // return post's id or false
+    return $post_id > 0 ? $post_id : false;
 };
