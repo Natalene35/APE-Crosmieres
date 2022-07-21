f792b3e84988c2e41b435adf49dfb4829563f861
Natalene35 <poupouille20@msn.com>
2022-07-21 16:58:34 +0200

    SaleDetailLayout creation

---
+++ frontend/src/components/sales/SaleDetailLayout.vue
@@ -0,0 +1,111 @@
+<template>
+    <section class="event--card">
+        <h2 class="event--card__title">{{ title }}</h2>
+
+        <div class="event--card__date">Date de la vente</div>
+
+        <img class="img" v-bind:src="tilleulPic">
+        <div class="media-image" v-bind:style="'background-image:url(' + image + ')'"></div>
+
+        <div class="event--card__content" v-html="content"></div>
+
+        <div>Lien de la vente</div>>
+    </section>
+</template>
+
+<script>
+import SaleService from "@/services/sales/SaleService";
+import tilleulPic from '@/assets/images/tilleul.jpg';
+import mapPic from '@/assets/images/map.jpg'
+export default {
+    name: "SaleDetailLayout",
+    data() {
+        return {
+            tilleulPic: tilleulPic,
+            mapPic: mapPic,
+            title: null,
+            date: null,
+            content: null,
+            image: null,
+            link: null
+        }
+    },
+    async mounted() {
+        // Allow to retrieve the id dynamic parameter by using the $route object
+        let id = this.$route.params.id;
+        const response = await SaleService.find(id);
+        if (response.code) {
+            // IL y a une erreur
+            alert(response.message);
+            // @TODO Ajouter une redirection vers l'accueil avec un message d'erreur
+        } else {
+            this.title = response.title.rendered;
+            /* this.date = response.date.rendered;
+            this.location = response.location.rendered; */
+            this.content = response.content.rendered;
+            this.image = response._embedded['wp:featuredmedia'] ? response._embedded['wp:featuredmedia'][0].source_url : 'https://source.unsplash.com/collection/157&random=100';
+            console.log(this.image)
+            this.link = response.link.rendered;
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.event--card {
+    color: $black;
+    border: 1px solid $black;
+    width: 70%;
+    margin: auto;
+    height: auto;
+    padding: 1rem;
+    border-radius: 1rem;
+
+    h2 {
+        font-size: 2rem;
+        font-weight: bold;
+        padding-bottom: 1rem;
+    }
+
+    .event--card__date,
+    .event--card__location,
+    .event--card__img,
+    .event--card__content,
+    .event--card__map {
+        padding: 1rem;
+    }
+
+    .event--card__img {
+        width: 100%;
+
+        height: auto;
+        margin: auto;
+    }
+
+    .event--card__map {
+        width: 40%;
+        margin: auto;
+    }
+
+    .event--card__content {
+        width: 100%;
+    }
+
+    .img {
+        width: 58%;
+        height: auto;
+        border-radius: 1em 1em 0 0;
+        -o-object-fit: cover;
+        object-fit: cover;
+        transform: translateY(-2%);
+    }
+
+    .media-image {
+        min-height: 300px;
+        background-position: center;
+        background-size: cover;
+        border-radius: 1rem;
+        margin-bottom: 1rem;
+    }
+}
+</style>
