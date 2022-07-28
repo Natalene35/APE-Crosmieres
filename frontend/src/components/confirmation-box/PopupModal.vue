7a4322e56b578e4d221c23d64102f4aa996bc01b
Natalene35 <poupouille20@msn.com>
2022-07-28 22:02:51 +0200

    Confirmation popup creation

---
+++ frontend/src/components/confirmation-box/PopupModal.vue
@@ -0,0 +1,67 @@
+<template>
+    <transition name="fade">
+        <div v-if="isVisible">
+            <div>
+                <slot></slot>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'PopupModal',
+
+    data: () => ({
+        isVisible: false,
+    }),
+
+    methods: {
+        open() {
+            this.isVisible = true
+        },
+
+        close() {
+            this.isVisible = false
+        },
+    },
+}
+
+</script>
+
+<style lang="scss" scoped>
+/* scss class for the transition */
+// animation for the opacity when the popup closes and opens
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 0.3s;
+}
+
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
+
+.popup-modal {
+    background-color: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 0.5rem;
+    display: flex;
+    align-items: center;
+    z-index: 1;
+}
+
+.window {
+    background: $white;
+    border-radius: 5px;
+    box-shadow: 2px 4px 8px rgba(0, 0, 0, 0.2);
+    max-width: 480px;
+    margin-left: auto;
+    margin-right: auto;
+    padding: 1rem;
+}
+</style>
