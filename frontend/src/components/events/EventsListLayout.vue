056d69214818d2cdbc1f708d296a8f724e28ba81
EmiliePotier <emilie.potier@outlook.fr>
2022-07-28 14:20:44 +0200

    sccs search bar

--- frontend/src/components/events/EventsListLayout.vue
+++ frontend/src/components/events/EventsListLayout.vue
@@ -1,15 +1,16 @@
 <template>
-<div>
-    <!-- Load icon library from font awesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-    <button class="search-icon"> <i class="fa fa-search"></i></button> 
-    <input type="text" placeholder="Rechercher un évènement..." v-model="searchString">
-
     <section class="event--section">
+        <div class="event--section__search">
         <img class="event--card__img" v-bind:src="eventPicture">
+        <!-- Load icon library from font awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
+        <button class="search-icon"> <i class="fa fa-search"></i></button> 
+        <input type="text" placeholder="Rechercher..." v-model="searchString">
+        
+        </div>
         <EventListLayout v-bind:image="event.featured_media !== 0 ? event._embedded['wp:featuredmedia'][0].source_url : 'https://source.unsplash.com/collection/157&random=100'" v-bind:id="event.id" v-bind:title="event.title.rendered" v-bind:content="event.content.rendered" v-for="event in eventsNewList" v-bind:key="event.id"/>
     </section>
-</div>
+
 </template>
 
 
@@ -63,37 +64,42 @@ export default {
 
 <style scoped lang="scss">
 
-/* Style the search field */
-input {
-padding: 11px;
-font-size: 15px;
-border-style: none;
-box-shadow: 1px 1px 1px $blue;
-border-radius : 10px;
-width: 25%;
-background: $white;
-}
-
-.search-icon {
-border-style:none;
-background-color: $grey;
-box-shadow: 1px 1px 1px rgba(88, 200, 235, 0.1);
-color:$white;
-height: 40px;
-width: 40px;
-border-radius:25%;
-margin-right: 5px;
-
-}
- 
-
 .event--section {
     display: flex;
     flex-direction: column;
     align-items: center;
 
+    .event--section__search {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    input {
+        padding: 11px;
+        font-size: 15px;
+        border-style: none;
+        width: 25%;
+        background: $white;
+        border: 1px solid $blue-bg-header;
+        border-radius: 0.5em;
+    }
+
+    .search-icon {
+        border-style:none;
+        background-color: $red;
+        color:$white;
+        height: 40px;
+        width: 40px;
+        border-radius:25%;
+        margin-right: 5px;
+        margin-left: 2rem;
+
+    }
+
     .event--card__img {
-            width: 6rem;
+        width: 6rem;
             
     }
 }
