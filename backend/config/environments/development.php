fc85ac1c6deec6de04a6c7af9d28ecb68f88e716
LoicDebruille <loic.debruille57@gmail.com>
2022-07-28 12:19:31 +0200

    disable display wp theme

--- backend/config/environments/development.php
+++ backend/config/environments/development.php
@@ -15,7 +15,7 @@ Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
 Config::define('DISALLOW_INDEXING', true);
 Config::define('FS_METHOD', 'direct');
-//Config::define('WP_USE_THEMES', false);
+Config::define('WP_USE_THEMES', false);
 Config::define('JWT_AUTH_SECRET_KEY', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9hcGVjcm9zbWllcmVzLmxvY2FsIiwiaWF0IjoxNjU4MzkxNTkxLCJuYmYiOjE2NTgzOTE1OTEsImV4cCI6MTY1ODk5NjM5MSwiZGF0YSI6eyJ1c2VyIjp7ImlkIjoxLCJkZXZpY2UiOiIiLCJwYXNzIjoiMzBjYWU3ZTdhOTMzODllNmI3ZTIxODQwZDhiNzJkMDgifX19.h7TrQV7IaEJZT3uwabJZxM6C5cr7o85AAvETQcWd6Lc');
 
 
