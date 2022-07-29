756bbc26e8eac7e81c489cc7e981a67334e8de15
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-07-29 16:42:32 +0200

    add form and config send mail hotmail with insomnia ok

---
+++ backend/web/app/plugins/ape/custom-endpoints/mail.php
@@ -0,0 +1,131 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+// add function to the init hook
+add_action('rest_api_init', 'ape_rest_user_email');
+
+
+// create route WP for customize
+function ape_rest_user_email()
+{
+    register_rest_route('wp/v2', 'send', array(
+        'methods' => 'POST',
+        'callback' => 'ape_rest_user_send_email',
+        'permission_callback' => function () {
+            return true;
+        }
+    ));
+}
+
+
+function ape_rest_user_send_email($request)
+{
+    // if (isset($_POST['name']) && isset($_POST['email'])) {
+    //     $name = $_POST['name'];
+    //     $email = $_POST['email'];
+    //     $subject = $_POST['subject'];
+    //     $body = $_POST['body'];
+
+
+
+    // require_once  plugin_dir_path(__FILE__) . "vendor/phpmailer/phpmailer/src/PHPMailer.php";
+    // require_once  plugin_dir_path(__FILE__) . "vendor/phpmailer/phpmailer/src/SMTP.php";
+    // require_once  plugin_dir_path(__FILE__) . "vendor/phpmailer/phpmailer/src/Exception.php";
+
+
+    $mail = new PHPMailer();
+
+
+    $email = "christophe.desmarres@hotmail.com";
+    $name = "titi";
+    $subject = "test";
+    $body = "test body";
+
+
+    //smtp settings
+    $mail->isSMTP();
+    $mail->Host = "smtp.office365.com";
+    $mail->SMTPAuth = true;
+    $mail->Username = "christophe.desmarres@hotmail.com";
+    $mail->Password = '';
+    $mail->Port = 587;
+    $mail->SMTPSecure = "STARTTLS";
+
+
+
+    //email settings
+    $mail->isHTML(true);
+    $mail->setFrom($email, $name);
+    $mail->addAddress("noursausore@gmail.com"); // destinataire
+    $mail->Subject = ("$email ($subject)");
+    $mail->Body = $body;
+
+    // return  $mail;
+
+    if ($mail->send()) {
+        $status = "success";
+        $response = "Email is sent!";
+    } else {
+        $status = "failed";
+        $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+    }
+
+    exit(json_encode(array("status" => $status, "response" => $response)));
+}
+
+
+
+function sendWithPhpMailer($subject, $body, $reply)
+{
+    require(ABSPATH . WPINC . '/class-phpmailer.php');
+    require(ABSPATH . WPINC . '/class-smtp.php');
+
+    // date_default_timezone_set( 'America/Sao_Paulo' );
+
+    $blogname = wp_strip_all_tags(trim(get_option('blogname')));
+    $smtpHost = wp_strip_all_tags(trim(get_option('smtp_host')));
+    $mailPort = wp_strip_all_tags(trim(get_option('smtp_port')));
+    $smtpUser = wp_strip_all_tags(trim(get_option('smtp_user')));
+    $smtpPass = wp_strip_all_tags(trim(get_option('smtp_pass')));
+    $mailTo = wp_strip_all_tags(trim(get_option('mail_to')));
+
+    $send = false;
+    $mail = new PHPMailer;
+
+    try {
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->Sender = $smtpUser;
+        $mail->CharSet = 'utf-8';
+        $mail->SMTPSecure = 'tls';
+        $mail->SMTPAuth = true;
+        $mail->Port = $mailPort;
+        $mail->Host = $smtpHost;
+        $mail->Username = $smtpUser;
+        $mail->Password = $smtpPass;
+        $mail->Subject = $subject;
+        $mail->From = $smtpUser;
+        $mail->setFrom($smtpUser, $blogname);
+        $mail->addReplyTo($reply);
+        $mail->addAddress($mailTo);
+
+        // Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');
+
+        $mail->isHTML(true);
+        $mail->Body = $body;
+
+        $send = $mail->Send();
+
+        $mail->ClearAllRecipients();
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: $mail->ErrorInfo \n";
+        echo "Error: $e";
+        return false;
+    }
+
+    return $send;
+}
