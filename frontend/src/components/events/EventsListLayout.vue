84fd760bd6ecb9a9ee9b75fc156db7d756a2d92b
EmiliePotier <emilie.potier@outlook.fr>
2022-07-29 18:10:21 +0200

    add scss for radio buttons

--- frontend/src/components/events/EventsListLayout.vue
+++ frontend/src/components/events/EventsListLayout.vue
@@ -1,25 +1,24 @@
 <template>
   <section class="event--section">
     <div class="event--section__search">
-      <img class="event--card__img" v-bind:src="eventPicture" />
+      <img class="event--img" v-bind:src="eventPicture" />
       <!-- Load icon library from font awesome -->
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-      />
+      <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
       <button class="search-icon"><i class="fa fa-search"></i></button>
-      <input type="text" placeholder="Rechercher..." v-model="searchString" />
+      <input type="text" class="search--input" placeholder="Rechercher..." v-model="searchString" />
 
-      <div>Choix du type d'évènement:</div>
-      <input type="radio" id="all" v-model="picked" value="all" />
-      <label for="statement">Tous</label>
+      <div class="button--title"></div>
+      <input type="radio" class="button--radio" id="all" v-model="picked" value="all" />
+      <label class="button--radio__title" for="all">Tout</label>
       <!-- value 3 = id of taxonomie statement "réunion in backoffice wp" -->
-      <input type="radio" id="statement" value="3" v-model="picked" />
-      <label for="statement">Réunion</label>
+      <input type="radio" class="button--radio" id="statement" value="3" v-model="picked" />
+      <label class="button--radio__title" for="statement">Réunions</label>
       <!-- value 4 = id of taxonomie actuality "actualité in backoffice wp" -->
-      <input type="radio" id="actuality" value="4" v-model="picked" />
-      <label for="actuality">Actualité</label>
+      <input type="radio" class="button--radio" id="actuality" value="4" v-model="picked" />
+      <label class="button--radio__title" for="actuality">Actualités</label>
     </div>
+
     <EventListLayout
       v-bind:image="
         event.featured_media !== 0
@@ -59,7 +58,7 @@ export default {
       return this.eventsList.filter((event) => {
         // We take the title of the current event and we check if the searched term is contained in this title.
         // If yes, return true
-        console.log("ok");
+        // search possible in lowercase and without accent
         if(this.picked == "all"){
             if (event.title.rendered.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").includes(this.searchString.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase())) {
                 return true;
@@ -82,7 +81,7 @@ export default {
       eventsList: [],
       eventPicture: picture,
       searchString: "",
-
+      //default value for radio buttons 
       picked: "all",
     };
   },
@@ -102,7 +101,7 @@ export default {
     justify-content: center;
   }
 
-  input {
+  .search--input {
     padding: 11px;
     font-size: 15px;
     border-style: none;
@@ -120,10 +119,24 @@ export default {
     width: 40px;
     border-radius: 25%;
     margin-right: 5px;
-    margin-left: 2rem;
+    margin-left: 1rem;
   }
 
-  .event--card__img {
+  .button--radio {
+    
+  }
+
+  .button--title {
+    color: $grey;
+    padding-left: 3px;
+  }
+
+  .button--radio__title {
+    color: $grey;
+    
+  }
+
+  .event--img {
     width: 6rem;
   }
 }
