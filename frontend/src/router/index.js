f90ac9c1cf43092b28bcf9ffd9c5e4baaf25ae2d
LoicDebruille <loic.debruille57@gmail.com>
2022-08-02 15:50:09 +0200

    modification name URL in french

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,110 +8,103 @@ import HomeView from '../views/HomeView.vue'
 import store from '@/store/index'
 
 const routes = [{
-    path: '/',
+    path: '/accueil',
     name: 'home',
     component: HomeView
   },
 
   //Road for login
   {
-    path: '/login',
+    path: '/connexion',
     name: 'login',
     component: () => import( /* webpackChunkName: "login" */ '../views/login/UserLoginView.vue')
   },
 
   // route for creating event
   {
-    path: '/event/create',
+    path: '/creez-vos-evenements',
     name: 'eventCreate',
     component: () => import( /* webpackChunkName: "create-event" */ '../views/events/EventCreateView.vue')
   },
    // route for update sale
    {
-    path: '/event/update/:id',
+    path: '/modifier-votre-evenement/:id',
     name: 'eventUpdate',
     component: () => import( /* webpackChunkName: "create-sale" */ '../views/events/EventUpdateView.vue')
   },
 
   // route for creating sale
   {
-    path: '/sale/create',
+    path: '/creez-vos-ventes',
     name: 'saleCreate',
     component: () => import( /* webpackChunkName: "create-sale" */ '../views/sales/SaleCreateView.vue')
   },
    // route for update sale
    {
-    path: '/sale/update/:id',
+    path: '/modifier-votre-vente/:id',
     name: 'saleUpdate',
     component: () => import( /* webpackChunkName: "create-sale" */ '../views/sales/SaleUpdateView.vue')
   },
 
   //Road for registration
   {
-    path: '/registration',
+    path: '/inscription',
     name: 'registration',
     component: () => import( /* webpackChunkName: "login" */ '../views/registration/UserRegisterView.vue')
   },
 
   //Road for events list
   {
-    path: '/events/list',
+    path: '/liste-des-evenements',
     name: 'eventsList',
     component: () => import( /* webpackChunkName: "events-list" */ '../views/events/EventsListView.vue')
   },
 
   //Road for sales list
   {
-    path: '/sales/list',
+    path: '/liste-des-ventes',
     name: 'salesList',
     component: () => import( /* webpackChunkName: "sales-list" */ '../views/sales/SalesListView.vue')
   },
 
   //Road for an event
   {
-    path: '/event/:id',
+    path: '/detail-de-cet-evenement/:id',
     name: 'event',
     component: () => import(/* webpackChunkName: "event" */ '../views/events/EventDetailView.vue')
   },
  
   //Road for a sale
   {
-    path: '/sale/:id',
+    path: '/detail-de-cette-vente/:id',
     name: 'sale',
     component: () => import(/* webpackChunkName: "sale" */ '../views/sales/SaleDetailView.vue')
   },
 
-  //Road for registration
-  {
-    path: '/registration',
-    name: 'registration',
-    component: () => import( /* webpackChunkName: "login" */ '../views/registration/UserRegisterView.vue')
-  },
-
   //Road for user profil
   {
-    path: '/userprofil',
+    path: '/mon-profil',
     name: 'userprofil',
     component: () => import( /* webpackChunkName: "login" */ '../views/user/ProfilView.vue')
   },
 
   // route for creating event
   {
-    path: '/member',
+    path: '/presentation-des-membre-APE',
     name: 'memberList',
     component: () => import( /* webpackChunkName: "member-list" */ '../views/user/MemberView.vue')
   },
 
   //Road for 404 page
   {
-    path: '/404',
+    path: '/erreur-404',
     name: '404',
     component: () => import( /* webpackChunkName: "404" */ '../views/errors/Page404View.vue')
   },
 
    //Road for back-office
    {
-    path: '/back-office',
+    path: '/administration',
     name: 'back-office',
      component: () => import(/* webpackChunkName: "login" */ '../views/back-office/BackOfficeView.vue')
   },
