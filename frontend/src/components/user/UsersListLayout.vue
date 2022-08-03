0d8abbde64e08e8d8dce438c0305ed81db4b700f
Natalene35 <poupouille20@msn.com>
2022-08-03 15:38:03 +0200

    Add new popup to delete, add phone, rename role and improvement scss

--- frontend/src/components/user/UsersListLayout.vue
+++ frontend/src/components/user/UsersListLayout.vue
@@ -8,107 +8,172 @@
             <link rel="stylesheet"
                 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <button class="search-icon"> <i class="fa fa-search"></i></button>
-            <input type="text" placeholder="Rechercher..." v-model="searchString">
+            <input type="text" placeholder="Rechercher par nom" v-model="searchString">
         </div>
         <ul>
             <li v-for="user in usersNewList" v-bind:key="user.id">
                 <div class="detail">
-                    <div>{{ user.last_name }} {{ user.first_name }}</div>
-                    <div>{{ user.email }}</div>
-                    <div>{{ user.roles[0] }}</div>
+                    <div>
+                        <p>{{ user.last_name }} {{ user.first_name }}</p>
+                        <p>{{ user.email }}</p>
+                        <p>{{ user.phone }}</p>
+                    </div>
+                    <div>
+                        <!-- <p class="role">Rôle : {{ user.roles[0] }}</p> -->
+                        <p class="role" v-if="user.roles[0] === 'administrator'">Rôle : Administrateur</p>
+                        <p class="role" v-if="user.roles[0] === 'apeuser'">Rôle : Utilisateur APE</p>
+                        <p class="role" v-if="user.roles[0] === 'apemember'">Rôle : Membre APE</p>
+                    </div>
                 </div>
                 <!-- Only the admin can modify the users list -->
                 <div v-if="this.$store.getters.getRole === 'administrator'" class="change">
-                    <!-- This button calls the chooseARole methods et make appears the selected form -->
-                    <button v-on:click="chooseARole(user.id)">Modifier le rôle</button>
+
+                    <!-- Calls the chooseARole methods et make appears the selected form -->
+                    <img v-on:click="chooseARole(user)" v-bind:src="edit" class="picture"
+                        title="Modifier le rôle de ce compte" />
 
                     <!-- Calls a popup to confirm the user's delete -->
-                    <img v-on:click="opacity = 1; zindex = 20; selectUserId = user.id" class="picture"
-                        title="Supprimer ce compte" alt="trash" v-bind:src="trash" />
+                    <img v-on:click="deleteUserConfirm(user)" class="picture" title="Supprimer ce compte" alt="trash"
+                        v-bind:src="trash" />
                 </div>
             </li>
         </ul>
 
-        <PopUpLayout v-bind:id="selectUserId" v-bind:opacity="this.opacity" v-bind:zindex="this.zindex"
-            v-on:yes="deleteById" v-on:no="this.opacity = 0, this.zindex = -20" />
-
         <!-- The selected form appears on the click of the button by the chooseARole method -->
         <div class="select" v-if="showSelected">
-            <label for="role-select">Choisir un nouveau rôle </label>
-            <select name="role" id="role-select" v-model="selectedRole">
-                <option value="apemember">apemember</option>
-                <option value="apeuser">apeuser</option>
-                <option value="administrator">administrator</option>
-            </select>
-
-            <input type="submit" v-on:click="updateRole()" value="Modifier le rôle">
+            <div class="select--wrapper">
+                <div class="select--container">
+                    <label for="role-select">Le rôle de {{ userToChange.name }} est {{ userToChange.roles[0] }}. Voulez
+                        vous le modifier ?</label>
+                    <select name="role" id="role-select" v-model="selectedRole">
+                        <option value="apemember">Membre APE</option>
+                        <option value="apeuser">Utilisateur APE</option>
+                        <option value="administrator">Administrateur</option>
+                    </select>
+                    <div>
+                        <input class="selectButton selectButtonConfirm" type="submit" v-on:click="updateRole()"
+                            value="Modifier le rôle" />
+                        <input class="selectButton selectButtonAbort" type="submit" v-on:click="showSelected = false"
+                            value="Annuler" />
+                    </div>
+                </div>
+            </div>
         </div>
+
+
+        <div class="select" v-if="showDeleted">
+            <div class="select--wrapper">
+                <div class="select--container">
+                    <label for="role-select">Etes vous sûr de vouloir supprimer le compte de {{ userToDelete.name }}
+                        ?</label>
+                    <input class="selectButton selectButtonConfirm" v-on:click="deleteById(userToDelete)"
+                        value="Supprimer le compte" />
+                    <input class="selectButton selectButtonAbort" v-on:click="showDeleted = false" value="Annuler" />
+                </div>
+            </div>
+        </div>
+
         <!-- display a succes message if the role is correctly modified -->
-        <p class="succesUpdate" v-for="succesMsg in succesUpdate" v-bind:key="succesMsg">
+
+        <p class="successUpdate" v-for="succesMsg in succesUpdate" v-bind:key="succesMsg">
             {{ succesMsg }}
         </p>
+
+
     </section>
 </template>
 
 <script>
-import UserloginService from '@/services/login/UserLoginService';
+import UserLoginService from '@/services/login/UserLoginService';
 import UserService from '@/services/user/UserService';
 import trash from '@/assets/images/icons8-trash-can-100.png';
-import PopUpLayout from '@/components/back-office/PopUpLayout';
+import edit from '@/assets/images/icons8-edit-100.png';
 
 export default {
     name: 'UsersListLayout',
-    components: {
-        PopUpLayout
-    },
 
     data() {
         return {
             users: [],
             trash: trash,
+            edit: edit,
             selectedRole: null,
             errors: [],
             succesUpdate: [],
             showSelected: false,
-            idToChange: null,
-            zindex: -20,
-            opacity: 0,
+            showDeleted: false,
+            userToChange: null,
+            userToDelete: null,
             selectUserId: null,
             searchString: "",
         }
     },
     async mounted() {
         // Contain the users list returns by the API
-        this.users = await UserloginService.findAll();
-        console.log(this.users);
+        this.users = await UserLoginService.findAll();
+        // to add the phone meta, we browse all users
+        this.users.forEach(async (user) => {
+            let phone = "";
+            // for current user, we retrieve the meta data
+            let arrayMeta = await UserLoginService.getMeta(user.id);
+            for (let index = 0; index < arrayMeta.length; index++) {
+                const metaElmt = arrayMeta[index];
+                // when the meta key match with this meta desired
+                // we put value in variable 'phone' in this case
+                if (metaElmt.meta_key == "phone") {
+                    phone = metaElmt.meta_value;
+                }
+            }
+            // we add meta key and meta value to the current user object for use later
+            user["phone"] = phone;
+        });
     },
 
     methods: {
-        async deleteById(e) {
-            this.opacity = 0
-            this.zindex = -20
-            const response = await UserService.deleteById(e);
-            this.users = await UserloginService.findAll();
-            console.log(response);
+
+        deleteUserConfirm(user) {
+            // console.log(user);
+            this.showDeleted = true;
+            this.userToDelete = user;
         },
 
-        chooseARole(id) {
-            // console.log(id);
+        async deleteById(user) {
+            const response = await UserService.deleteById(user.id);
+            // console.log(response);
+            if (response) {
+                this.succesUpdate.push("Le compte de " + response.previous.name + " a bien été supprimé");
+                setTimeout(() => {
+                    this.succesUpdate = [];
+                }, 5000);
+            } else {
+                this.errors.push("Le compte n'a pas été supprimé");
+                setTimeout(() => {
+                    this.errors = [];
+                }, 5000);
+            }
+            this.users = await UserLoginService.findAll();
+            this.userToDelete = null;
+            this.showDeleted = false;
+        },
+
+        chooseARole(user) {
+            // console.log(user);
             this.showSelected = true;
-            this.idToChange = id;
+            this.userToChange = user;
         },
 
         async updateRole() {
             // If the connected person is the administrator
             if (this.$store.getters.getRole === "administrator") {
-                const response = await UserService.update(this.idToChange,
+                const response = await UserService.update(this.userToChange.id,
                     {
                         roles: [this.selectedRole]
                     });
+                console.log(response);
                 if (response.id) {
                     // console.log(response);
-                    this.users = await UserloginService.findAll();
-                    this.succesUpdate.push("Le statut de " + response.name + "a bien été modifié");
+                    this.users = await UserLoginService.findAll();
+                    this.succesUpdate.push("Le rôle de " + response.name + " a bien été modifié");
                     setTimeout(() => {
                         this.succesUpdate = [];
                     }, 5000);
@@ -119,7 +184,23 @@ export default {
                     }, 5000);
                 }
             }
+            this.userToChange = null;
             this.showSelected = false;
+        },
+
+        async getMeta(id) {
+            //retrieve logged in user meta data
+            let arrayMeta = await UserLoginService.getMeta(id);
+            // console.log(arrayMeta);
+            for (let index = 0; index < arrayMeta.length; index++) {
+                const metaElmt = arrayMeta[index];
+                //For take meta_key enter key in the exemple its "phone"
+                if (metaElmt.meta_key == "phone") {
+                    return metaElmt.meta_value;
+                } else {
+                    return "";
+                }
+            }
         }
     },
 
@@ -145,10 +226,16 @@ export default {
 section {
     color: $grey;
     margin-bottom: 2rem;
+    position: relative;
 
     h1 {
-        font-size: 2rem;
-        margin-bottom: 2rem;
+        color: $purple;
+        font-size: 1.5rem;
+        font-weight: bold;
+        font-family: 'Merienda', cursive;
+        margin-top: 0.5rem;
+        margin-bottom: 1rem;
+        text-shadow: 0px 0px 0px $grey;
     }
 
     // CSS for the search box
@@ -157,13 +244,14 @@ section {
         display: flex;
         align-items: center;
         justify-content: center;
+        margin-bottom: 2rem;
     }
 
     input {
         padding: 11px;
         font-size: 15px;
         border-style: none;
-        width: 25%;
+        width: 50%;
         background: $white;
         border: 1px solid $blue-bg-header;
         border-radius: 0.5em;
@@ -183,20 +271,15 @@ section {
 
     // SCSS for the li boxes
     li {
-        border: 1px solid $black;
         background-color: $white;
-        width: 90%;
+        width: 100%;
         height: auto;
         margin: 0.2rem auto;
         border-radius: 1rem;
         display: flex;
         justify-content: space-between;
         align-items: center;
-
-        .select {
-            display: flex;
-            flex-direction: column;
-        }
+        box-shadow: 0px 17px 34px -20px $blue-bg-header;
 
         .picture {
             height: 3rem;
@@ -208,17 +291,122 @@ section {
             transform: scale(1.2);
         }
 
+
         .change {
-            display: flex;
-            align-items: center;
             margin-right: 1rem;
+        }
+
+        p {
+            text-align: left;
+            margin-bottom: 0.2rem;
+        }
+
+        .detail {
+            margin: 0.5rem 0rem 0.5rem 1rem;
         }
 
     }
 
-    @media (min-width: 1024px) {
+    // Select popup
+    .select {
+        position: fixed;
+        font-size: 1.5rem;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        display: table;
+        transition: opacity 0.3s ease;
+
+        .select--wrapper {
+            display: table-cell;
+            vertical-align: middle;
+
+            .select--container {
+                display: flex;
+                flex-direction: column;
+                width: 18rem;
+                margin: auto;
+                padding: 20px 30px;
+                background-color: $white;
+                border-radius: 1rem;
+                box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+                transition: all 0.3s ease;
+
+                .selectButton {
+                    display: inline-block;
+                    width: 80%;
+                    font-size: 1.2rem;
+                    padding: 0.5em;
+                    margin: 0.5rem auto;
+                    text-align: center;
+                    border-radius: 5px;
+                    box-shadow: 0 5px 5px $black;
+                    cursor: pointer;
+                }
+
+                .selectButtonConfirm {
+                    color: $green;
+                    background-color: $white;
+                    border: 1px solid $green;
+                }
+
+                .selectButtonAbort {
+                    color: $red;
+                    background-color: $white;
+                    border: 1px solid $red;
+                }
+
+                .selectButtonConfirm:hover {
+                    color: $white;
+                    background-color: $green;
+                    border: 1px solid $white;
+                }
+
+                .selectButtonAbort:hover {
+                    color: $white;
+                    background-color: $red;
+                    border: 1px solid $white;
+                }
+
+                #role-select {
+                    width: 50%;
+                    height: 1.5rem;
+                    margin: 0.5rem auto;
+                }
+            }
+        }
+    }
+
+    .successUpdate {
+        z-index: 1;
+        background: $blue-bg-header;
+        font-size: 1.1rem;
+        padding: 1rem 1rem;
+        width: 18rem;
+        margin: auto;
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        border-left: 8px solid $green;
+        border-radius: 4px;
+        transform: translateX(-50%)
+    }
+
+
+    @media (min-width: 768px) {
+        input {
+            width: 20%;
+        }
         li {
             width: 60%;
+
+            .picture {
+                height: 4rem;
+                cursor: pointer;
+            }
         }
     }
 }
