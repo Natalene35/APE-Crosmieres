f14eb4c35a7a59b54ce09db86ac07635177ec880
Christophe-Desmarres <chrisdmar12@gmail.com>
2022-08-04 10:42:10 +0200

    add favicon and page title

--- frontend/src/views/sales/SalesListView.vue
+++ frontend/src/views/sales/SalesListView.vue
@@ -1,32 +1,30 @@
 <template>
-
-        <HeaderLayout/>
-        <SalesListLayout/>
-        <FooterLayout/>
-  
+  <HeaderLayout />
+  <SalesListLayout />
+  <FooterLayout />
 </template>
 
 
 <script>
-import HeaderLayout from '@/components/templates/HeaderLayout.vue';
-import SalesListLayout from '@/components/sales/SalesListLayout.vue';
-import FooterLayout from '@/components/templates/FooterLayout.vue';
+import HeaderLayout from "@/components/templates/HeaderLayout.vue";
+import SalesListLayout from "@/components/sales/SalesListLayout.vue";
+import FooterLayout from "@/components/templates/FooterLayout.vue";
 
 export default {
-    name: "SalesListView",
-    components: {
-        HeaderLayout,
-        SalesListLayout,
-        FooterLayout,
-        
-    }
-}
-
+  name: "SalesListView",
+  components: {
+    HeaderLayout,
+    SalesListLayout,
+    FooterLayout,
+  },
+  mounted() {
+    //for change title view page
+    document.title = "Ventes";
+  },
+};
 </script>
 
 
 <style scoped lang="scss">
-
-
 </style>
 
